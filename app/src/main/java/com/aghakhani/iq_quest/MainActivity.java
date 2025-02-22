967f9fd3ef63be52404e1a2b85480e678c4e43bb
Kiarash Aghakhani <kiarash88ca@gmail.com>
2025-02-21 20:38:09 -0800

    Add 20 questions to the program

--- app/src/main/java/com/aghakhani/iq_quest/MainActivity.java
+++ app/src/main/java/com/aghakhani/iq_quest/MainActivity.java
@@ -29,23 +29,54 @@ public class MainActivity extends AppCompatActivity {
     // Quiz data
     private String[] questions = { // Array of questions
             "What is the next number in the sequence: 2, 4, 8, 16, ...?",
-            "If all Bloops are Razzies and all Razzies are Lazzies, are all Bloops definitely Lazzies?",
-            "Which word is the odd one out?",
+            "If all cats are animals and some animals are dogs, are some cats dogs?",
+            "Which word is the odd one out? (Book, Notebook, Pencil, Television)",
             "How many squares are there in the given figure? (Imagine a large square with smaller squares inside)",
             "Ali is twice as old as Reza. Five years later, Reza will be 15. How old is Ali now?",
-            "If one clock is 3 minutes slow and another is 2 minutes fast, what is the time difference?"
+            "If one clock is 3 minutes slow and another is 2 minutes fast, what is the time difference?",
+            "Which shape comes next in the sequence? (Imagine a visual pattern changing)",
+            "If 5 people stand in a line, how many different arrangements are possible?",
+            "The statement 'All birds fly' is incorrect because:",
+            "What is the next number in the sequence 1, 3, 6, 10, ...?",
+            "A man has 4 daughters, and each daughter has 1 brother. How many children does he have?",
+            "If today is Thursday, what day will it be 45 days from now?",
+            "Which word is different from the rest? (Book, Notebook, Pencil, Television)",
+            "5, 10, 20, 40, ... What is the next number?",
+            "The sentence 'All humans die, Socrates is human, therefore...' completes as:",
+            "How many possible 4-digit passwords can be made using only 1, 2, and 3?",
+            "In a room of 7 people, each person shakes hands with everyone else. How many handshakes occur?",
+            "What is the next number in the Fibonacci sequence: 1, 1, 2, 3, 5, ...?",
+            "'If it rains, the street gets wet.' Which statement is true?",
+            "The ratio of 2 to 8 is similar to the ratio of 3 to ...?",
+            "If A is greater than B and B is greater than C, which statement is true?"
     };
 
     private String[][] options = { // 2D array of answer options for each question
             {"24", "32", "20", "30"},
             {"Yes", "No", "Maybe", "Cannot be determined"},
-            {"Dog", "Cat", "Table", "Bird"},
+            {"Book", "Notebook", "Pencil", "Television"},
             {"9", "10", "11", "12"},
             {"30", "25", "35", "40"},
-            {"1 minute", "5 minute", "3 minute", "6 minute"}
+            {"1 minute", "5 minutes", "3 minutes", "6 minutes"},
+            {"Shape A", "Shape B", "Shape C", "Shape D"},
+            {"120", "60", "24", "20"},
+            {"Some birds do not fly", "All birds fly", "Some birds swim", "None of the above"},
+            {"12", "15", "16", "21"},
+            {"4", "5", "6", "8"},
+            {"Monday", "Friday", "Saturday", "Sunday"},
+            {"Book", "Notebook", "Pencil", "Television"},
+            {"60", "70", "80", "100"},
+            {"Socrates does not die", "Socrates is dead", "Socrates will die", "Socrates is an exception"},
+            {"12", "24", "81", "27"},
+            {"21", "42", "14", "35"},
+            {"8", "10", "12", "7"},
+            {"If the street is wet, it rained", "If the street is not wet, it did not rain", "If it does not rain, the street is not wet", "None of the above"},
+            {"9", "10", "12", "15"},
+            {"A is smaller than C", "A is equal to C", "A is greater than C", "Cannot be determined"}
     };
 
-    private int[] correctAnswers = {1, 0, 2,0,0,1}; // Array of correct answer indices corresponding to each question
+    private int[] correctAnswers = {1, 1, 3, 0, 0, 1, 1, 0, 0, 1, 1, 0, 3, 2, 2, 2, 0, 0, 2, 2, 2}; // Array of correct answer indices corresponding to each question
+
 
     // Quiz state
     private int currentQuestionIndex = 0; // Index of the current question being displayed
@@ -79,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
                 if (selectedId == -1) {
                     // Show a message if no answer is selected
                     // Toast.makeText(MainActivity.this, "Please select an answer", Toast.LENGTH_SHORT).show();
-                    custom_toast( "Please select an answer");
+                    custom_toast("Please select an answer");
                     return;
                 }
 
